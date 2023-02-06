91df1a8ec7c9c742a28a52c55da23fd4f128ae5b
TranLeHaiDuong <haiduongtlh2000@gmail.com>
2023-02-06 14:04:18 +0700

    server

--- client/src/component/Announcement.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Container } from "./style-component/Announcement";
-
-const Announcement = () => {
-  return <Container>Super Deal! Free Shipping on Orders Over $50</Container>;
-};
-
-export default Announcement;
